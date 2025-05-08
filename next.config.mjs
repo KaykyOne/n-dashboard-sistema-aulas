1124b8a116f19b45e4216106b264d0c071e8f4b7
KaykyOne <kaykyzioti@gmail.com>
2025-05-08 07:12:12 -0300

    teste

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  ...(isProd && {
+    output: 'export',
+    basePath: '/n-dashboard-sistema-aulas',
+    trailingSlash: true,
+  }),
+};
 
 export default nextConfig;
